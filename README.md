21a98e091d2dd3d2f618d643ae19d54d4b44c424
Adam <gienappa@gmail.com>
2020-09-17 13:21:04 -0700

    3rd time's the charm

---
+++ README.md
@@ -0,0 +1,6 @@
+Personal Website ver. 2.0
+
+Notes: Second build of personal website (http://adamgienapp.com)
+
+Tech stack:
+React, Node/Express
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const nodemailer = require('nodemailer');
+const config = require('../nodemailer.config');
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.post('/send', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    auth: {
+      user: configs.acct,
+      pass: configs.pw
+    }
+  });
+
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: `${configs.acct}`,
+    subject: 'Message received from adamgienapp.com',
+    text: `Contact: ${req.body.name} \n
+          Email: ${req.body.email} \n
+          Subject: ${req.body.subject} \n
+          Message: ${req.body.message}`,
+    replyTo: `${req.body.email}`
+  }
+  
+  transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      res.status(401).send('Error sending email');
+    } else {
+      res.status(201).send('Email sent successfully');
+    }
+    transporter.close();
+  });
+});
+
+app.use('/', express.static(__dirname + '/../public'));
+app.use('/:endpoint', express.static(__dirname + '/../public'));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
