c246c779edf99d26a2133adce26d8cd3e396a378
Adam <gienappa@gmail.com>
2020-09-17 13:59:06 -0700

    App.js init Aux.js init index.html: revise/add metadata

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Aux from './hoc/Aux';
+
+export default class App extends Component{
+  render() {
+    return(
+      <Aux>
+        
+      </Aux>
+    );
+  }
+}
