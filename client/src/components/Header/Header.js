91f277a0fd391f98d7edc6d3c168074cb3993b61
Adam <gienappa@gmail.com>
2020-09-21 19:42:48 -0700

    - created UseWindowDimensions hook - Header:   - bg img changes according to viewport ratio   - arrow position given finer position breakpoints   - TO-DO:     - fix background image position, and then finished!

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,19 +2,32 @@
 import React from 'react';
 // Components
 import classes from './Header.css';
+import useWindowDimensions from '../../hooks/UseWindowDimensions';
+
 
 const Header = (props) => {
+  const { height, width } = useWindowDimensions();
+
+  let imgNum = 1;
+  if (width / height < 0.9) {
+    imgNum = 2;
+  }
 
   return (
-    <div className={classes.Header}>
+    <div className={classes.Header}
+      style={{
+        backgroundImage: `url(/assets/img/bg${imgNum}.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: '50% 20%',
+      }}>
       <div className={classes.Title}>
         Adam Gienapp
         <div className={classes.Subtitle}>
           <em>Full Stack Software Engineer</em>
         </div>
       </div>
-      <div className={classes.Arrow}>
-        <i className={`fa fa-angle-down`} aria-hidden="true"></i>
+      <div className={classes.ArrowContainer}>
+        <i className={`fa fa-angle-down ${classes.Arrow}`} aria-hidden="true"></i>
       </div>
     </div>
   );
