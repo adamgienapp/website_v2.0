3bcc313aa3a03cdcdb4b38b81b18947e14834d77
Adam <gienappa@gmail.com>
2020-09-30 15:40:31 -0700

    index.html:   - add metadata to enable web app behavior on ios   - add script to detect if Android/iOS and add 1px scroll on page load to hide toolbars

--- client/src/components/ProjectDetail/ProjectDetail.js
+++ client/src/components/ProjectDetail/ProjectDetail.js
@@ -4,11 +4,21 @@ import classes from './ProjectDetail.css';
 import Button from '../../UI/Button/Button';
 
 const ProjectDetail = (props) => {
+
+  let imageBlock = null;
+
+  if (Array.isArray(props.image)) {
+
+  } else {
+    imageBlock = (
+      <img src={props.image}></img>
+    );
+  }
+
   return (
     <section className={classes.ProjectDetail}>
       <div className="section-title">{props.title}</div>
 
-      
 
       <div style={{ textAlign: 'center', marginBottom: '20px' }}>
         <Button clicked={props.shift}>Back to projects</Button>
