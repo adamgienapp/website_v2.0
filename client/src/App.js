22ab7e0a904c178e4cc6e186a5b4ccaab98186bd
Adam <gienappa@gmail.com>
2020-10-08 15:09:07 -0700

    App:   - revised image preload line per new SDC file type

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Cockpit from './components/Cockpit/Cockpit';
 
 export default class App extends Component{
   componentDidMount() {
-    const imgURLs = ["https://i.imgur.com/Sc7qASD.gif", "/assets/img/sdc_1.svg", "/assets/img/sdc_2.svg", "/assets/img/sdc_3.svg", "/assets/img/DK53A3i4.jpeg", "/assets/img/portfolioV2.png"];
+    const imgURLs = ["https://i.imgur.com/Sc7qASD.gif", "/assets/img/sdc_1.png", "/assets/img/sdc_2.png", "/assets/img/sdc_3.png", "/assets/img/DK53A3i4.jpeg", "/assets/img/portfolioV2.png"];
     imgURLs.forEach((url) => {
       const img = new Image();
       img.src = url;
